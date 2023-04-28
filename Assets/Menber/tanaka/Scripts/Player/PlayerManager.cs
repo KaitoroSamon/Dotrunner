5d2da2e4ed6955cd7abec2c1a86f90302b85a274
tayano0106 <jpkgjgm@gmail.com>
2023-04-28 18:21:02 +0900

    0428作業

---
+++ Assets/Menber/tanaka/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+5-最後に色を変更した方向にプレイヤーを移動させる
+5.1-アニメーションの追加（ゆっくり移動させる）
+5.2-アイテムがあるマスを選択したらアイテムの取得、発動を行う
+6-もう一人のプレイヤーの動作を受け付ける
+7-特定のマスに乗ったら終了させる[相手の城にたどり着く際は塗ポイントぴったり使わないとゴールできない]
+8-プレイヤーのHPを管理する[相手と同じマスに乗ると攻撃(攻撃した側が次の自分のターン行動不可)]
+11-キャラ選択画面で受け取ったIDに合わせてプレイヤースキンを変更する
+*/
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Menber/tanaka/Scripts/Player/SelectTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+3-ゲームパッドの十字キー入力で選択できるようにする
+3.5-選択中のマスの方向にエフェクトを入れわかりやすいようにする
+4-Bキー入力後マスの色を変更する
+9-マスのない場所には移動できないようにする
+10-ターン終了ボタンをXキーに入れる
+*/
+
+public class SelectTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
