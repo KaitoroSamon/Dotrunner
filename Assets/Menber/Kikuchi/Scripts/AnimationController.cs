deb8e1b021c45dee66846010bb6898d983d197d0
kikuchitoya <21c1020024kt@edu.tech.ac.jp>
2023-05-12 17:33:25 +0900

    拡縮アニメーションの状態をenum型で列挙 enumの変数にアニメーションの状態を合わせるためのコントローラーを作成 Controllerの関数に変更する値を引数で渡したらアニメーションが変更されるようにしました。

--- Assets/Menber/Kikuchi/Scripts/AnimationController.cs
+++ Assets/Menber/Kikuchi/Scripts/AnimationController.cs
@@ -1,36 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static AnimationList;
 
 public class AnimationController : MonoBehaviour
 {
-    [SerializeField]
-    private Animator animator;
+
+    public Animator[] animator = new Animator[0];
 
     AnimationList animationList;
     void Start()
     {
         animationList = this.gameObject.GetComponent<AnimationList>();
-        StartCoroutine("AnimTest");
+        //StartCoroutine("AnimTest");//アニメーションの変更がちゃんとできるか確認用のコルーチン
     }
 
-    private void Update()
+    //public void AnimControll(AnimationList.CURSORANIMATION_TYPE cursorAnim_type, AnimationList.PLAYERANIMATION_TYPE playerAnim_type)
+ public void AnimControll(AnimationList.CURSORANIMATION_TYPE cursorAnim_type)
     {
-        if (animationList.anim_type == ANIMATION_TYPE.SELECTING)
+        animationList.cursorAnim_type = cursorAnim_type;
+        //animationList.playerAnim_type = playerAnim_type;
+
+        if (animationList.cursorAnim_type == AnimationList.CURSORANIMATION_TYPE.SELECTING)
         {
-            animator.SetBool("Selection", true);
+            animator[0].SetBool("Selection", true);
         }
-        else if (animationList.anim_type == ANIMATION_TYPE.BEFORESELECT
-             || animationList.anim_type == ANIMATION_TYPE.SELECTING)
+        else if (animationList.cursorAnim_type == AnimationList.CURSORANIMATION_TYPE.STILLNESS
+             || animationList.cursorAnim_type == AnimationList.CURSORANIMATION_TYPE.SELECTED)
         {
-            animator.SetBool("Selection", false);
+            animator[0].SetBool("Selection", false);
         }
+
+    //    if (animationList.playerAnim_type == AnimationList.PLAYERANIMATION_TYPE.AWAIT)
+    //    {
+    //        Debug.Log("Await");
+    //        //animator[値].SetBool("", );
+    //    }
+    //    else if (animationList.playerAnim_type == AnimationList.PLAYERANIMATION_TYPE.WALK)
+    //    {
+    //        Debug.Log("Walk");
+    //    }
     }
 
-    public IEnumerator AnimTest()
-    {
-        yield return new WaitForSeconds(5);
-        animationList.anim_type = ANIMATION_TYPE.SELECTING;
-    }
+    //public IEnumerator AnimTest()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //    AnimControll(AnimationList.CURSORANIMATION_TYPE.SELECTING,animationList.playerAnim_type);
+    //    yield return new WaitForSeconds(5);
+    //    AnimControll(AnimationList.CURSORANIMATION_TYPE.SELECTED, AnimationList.PLAYERANIMATION_TYPE.WALK);
+    //}
 }
--- Assets/Menber/Kikuchi/Scripts/AnimationList.cs
+++ Assets/Menber/Kikuchi/Scripts/AnimationList.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class AnimationList : MonoBehaviour
+public class AnimationList : MonoBehaviour //使えるか不明瞭ですがアニメーションのリストです。
 {
-    public enum ANIMATION_TYPE
+    public enum CURSORANIMATION_TYPE//カーソルのアニメーションリスト
     {
-        BEFORESELECT,
-        SELECTING,
-        SELECTED,
+        STILLNESS,//一番最初のアニメーションしない状態
+        SELECTING,//選択中で拡縮のアニメーションを行う状態
+        SELECTED,//選択が完了し再び停止する状態
     }
 
-    public ANIMATION_TYPE anim_type;
+    //public enum PLAYERANIMATION_TYPE //他にアニメーションが付くかわかりませんがテスト用に作成したプレイヤーのアニメーションリスト
+    //{
+    //    AWAIT,//プレイヤー待機中のアニメーション
+    //    WALK,//プレイヤーが動いてるときのアニメーション
+    //}
+
+    public CURSORANIMATION_TYPE cursorAnim_type;//アニメーションの種類を変更するための変数
+    //public PLAYERANIMATION_TYPE playerAnim_type;
     void Start()
     {
-        anim_type = ANIMATION_TYPE.BEFORESELECT;
+        cursorAnim_type = CURSORANIMATION_TYPE.STILLNESS;//変数初期化
+        //playerAnim_type = PLAYERANIMATION_TYPE.AWAIT;
     }
 
 }
