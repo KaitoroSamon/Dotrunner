52110aae8cad397ebb3b4568916644b46623f83c
横山雄太 <96154391+YokoyamaYuta@users.noreply.github.com>
2023-05-12 14:55:20 +0900

    画面遷移
    画面遷移ができるようにしました。

---
+++ Assets/hashimotoyura/ScenesManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("ChangeScene", 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+      
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
