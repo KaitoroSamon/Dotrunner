a5971cd3c83b1dee9e0908af7ad0fe88028a49b0
横山 <96154391+YokoyamaYuta@users.noreply.github.com>
2023-05-24 18:01:44 +0900

    アイテムの処理完成（仮） ※P2側の処理も作成しましたが、情報を反映する場所が見当たらなかったので、コメントアウトしています。

--- Assets/Menber/Miyata/Scripts/GameManager.cs
+++ Assets/Menber/Miyata/Scripts/GameManager.cs
@@ -16,14 +16,34 @@ public class GameManager : MonoBehaviour
     //最大塗り回数
     private int  maxMoveCounter = 3;
     //バケツ所持数
-    private int rePaint = 0;
+    public static int rePaint_p1 = 0; //横山一部変更
     PlayerManager playerManager;
+
+    //横山加筆
+    //ポーションの最大所持数
+    public int Portopn_limit = 9;
+    //ポーションの所持数
+    private int portopn_p1 = 0;
+    private int portopn_p2 = 0;
+    //行動回数アップ変数
+    private int move_up;
+    //バケツの最大所持数
+    public int RePaint_limit = 0;
+    //バケツの所持数
+    public static int rePaint_p2 = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         playerObj1 = GameObject.Find("Square");
         playerObj2 = GameObject.Find("Square (1)");
+
+        //横山加筆
+        //初期化
+        portopn_p1 = 0;
+        portopn_p2 = 0;
+        rePaint_p1 = 0;
+        rePaint_p2 = 0;
     }
 
     // Update is called once per frame
@@ -37,7 +57,7 @@ public class GameManager : MonoBehaviour
                 // プレイヤー１の攻撃
                 playerObj1.GetComponent<Player1>().Attack();//プレイヤー1の行動を呼び出す
 
-                playerManager.PlayertrunUpdate(player1Trun, maxMoveCounter, rePaint);
+                playerManager.PlayertrunUpdate(player1Trun, maxMoveCounter, rePaint_p1);  //横山一部変更
             }
             else
             {
@@ -52,10 +72,20 @@ public class GameManager : MonoBehaviour
     {
         if (player1Trun)
         {
+            //横山加筆
+            maxMoveCounter = 3;
+            move_up = maxMoveCounter + portopn_p1;
+            maxMoveCounter = move_up;
+
             player1Trun = false;
         }
         else
         {
+            //横山加筆
+            //maxMoveCounter = 3;
+            //move_up = maxMoveCounter + portopn_p2;
+            //maxMoveCounter = move_up;
+
             player1Trun = true;
         }
     }
@@ -63,15 +93,71 @@ public class GameManager : MonoBehaviour
     //ポーションをとった処理
     public void addMoveCounter()
     {
-
+        //横山加筆
+        //P1側の処理
+        if(player1Trun)
+        {
+            if(Portopn_limit > portopn_p1)
+            {
+                portopn_p1++;
+            }
+        }
+        /*
+        //P2側の処理
+        else
+        {
+            if (Portopn_limit > portopn_p2)
+            {
+                portopn_p2++;
+            }
+        }
+        */
     }
     //バケツをとった処理
     public void addRePaint()
     {
-
+        //横山加筆
+        //P1側の処理
+        if (player1Trun)
+        {
+            if (RePaint_limit > rePaint_p1)
+            {
+                //バケツの所持数が９じゃなかったら
+                if(rePaint_p1 != 9)
+                {
+                    rePaint_p1 += 2;
+                }
+                else
+                {
+                    rePaint_p1++;
+                }
+            }
+        }
+        /*
+        //P2側の処理
+        else
+        {
+            if (RePaint_limit > rePaint_p2)
+            {
+                //バケツの所持数が９じゃなかったら
+                if(rePaint_p2 != 9)
+                {
+                    rePaint_p2 += 2;
+                }
+                else
+                {
+                    rePaint_p2++;
+                }
+            }
+        }
+        */
     }
     public void arrivalDestination()
     {
         //リザルト処理
     }
 }
+
+//横山加筆
+//P2側に情報を反映させる処理が見当たらなかったので、
+//処理だけ書いてコメントアウトしています。
--- Assets/Menber/Miyata/Scripts/Map.cs
+++ Assets/Menber/Miyata/Scripts/Map.cs
@@ -94,6 +94,24 @@ public class Map : MonoBehaviour
                 }
             }
         }
+
+        //横山加筆
+        //アイテム生成の処理
+        //ポーション
+        for(int i = 1; i <= 10; i++)
+        {
+            CreateItem(0, 8, 0, 16, "3");
+        }
+        //バケツ
+        for (int i = 1; i <= 5; i++)
+        {
+            CreateItem(0, 8, 0, 16, "4");
+        }
+        //爆弾
+        for (int i = 1; i <= 5; i++)
+        {
+            CreateItem(3, 6, 0, 16, "5");
+        }
     }
 
     //田中加筆
@@ -105,15 +123,21 @@ public class Map : MonoBehaviour
         {
             for (int j = 0; j < ColumnNumber; j++)
             {
-                if (i == pos.x && j == pos.y)
+                //横山一部変更　jとiが逆だったので変更
+                if (i == pos.y && j == pos.x)
                 {
                     //隣接したマスに何もなければ何もしない
-                    if (dungeonMap[i+1,j] == "1" || dungeonMap[i, j+1] == "1" || dungeonMap[i - 1, j] == "1" || dungeonMap[i, j - 1] == "1" ||
-                        dungeonMap[i + 1, j] == "6" || dungeonMap[i, j + 1] == "6" || dungeonMap[i - 1, j] == "6" || dungeonMap[i, j - 1] == "6")
+                    if (dungeonMap[i,j+1] == "1" || dungeonMap[i+1, j] == "1" || dungeonMap[i, j - 1] == "1" || dungeonMap[i - 1, j] == "1" ||
+                        dungeonMap[i, j + 1] == "6" || dungeonMap[i + 1, j] == "6" || dungeonMap[i, j - 1] == "6" || dungeonMap[i - 1, j] == "6")
                     {
                         formerData = int.Parse(dungeonMap[i, j]);
                         //対応した色に塗る
                         dungeonMap[i, j] = "1";
+                        //横山加筆
+                        if (GameManager.rePaint_p1 >= 1)
+                        {
+                            GameManager.rePaint_p1--;
+                        }
                         paint = true;
                     }
                 }
@@ -131,6 +155,64 @@ public class Map : MonoBehaviour
                 break;
             case 5:
                 //爆弾の処理
+                //横山加筆
+                for (int i = 0; i < LineNumber; i++)
+                {
+                    for (int j = 0; j < ColumnNumber; j++)
+                    {
+                        if (i == pos.y && j == pos.x)
+                        {
+                            //上
+                            if (dungeonMap[i - 1, j] != "6")
+                            {
+                                dungeonMap[i - 1, j] = "1";
+                                Instantiate(Player1Square, new Vector3(-7.5f + j, 3.5f - (i - 1), 0), Quaternion.identity);
+                            }
+                            //下
+                            if (dungeonMap[i + 1, j] != "6")
+                            {
+                                dungeonMap[i + 1, j] = "1";
+                                Instantiate(Player1Square, new Vector3(-7.5f + j, 3.5f - (i + 1), 0), Quaternion.identity);
+                            }
+                            //左
+                            if (dungeonMap[i, j - 1] != "6" && j != 0)
+                            {
+                                dungeonMap[i, j - 1] = "1";
+                                Instantiate(Player1Square, new Vector3(-7.5f + (j - 1), 3.5f - i, 0), Quaternion.identity);
+                            }
+                            //左斜め上
+                            if (dungeonMap[i - 1, j - 1] != "6" && j != 0)
+                            {
+                                dungeonMap[i - 1, j - 1] = "1";
+                                Instantiate(Player1Square, new Vector3(-7.5f + (j - 1), 3.5f - (i - 1), 0), Quaternion.identity);
+                            }
+                            //左斜め下
+                            if (dungeonMap[i + 1, j - 1] != "6" && j != 0)
+                            {
+                                dungeonMap[i + 1, j - 1] = "1";
+                                Instantiate(Player1Square, new Vector3(-7.5f + (j - 1), 3.5f - (i + 1), 0), Quaternion.identity);
+                            }
+                            //右
+                            if (dungeonMap[i, j + 1] != "6" && j != 16)
+                            {
+                                dungeonMap[i, j + 1] = "1";
+                                Instantiate(Player1Square, new Vector3(-7.5f + (j + 1), 3.5f - i, 0), Quaternion.identity);
+                            }
+                            //右斜め上
+                            if (dungeonMap[i - 1, j + 1] != "6" && j != 16)
+                            {
+                                dungeonMap[i - 1, j + 1] = "1";
+                                Instantiate(Player1Square, new Vector3(-7.5f + (j + 1), 3.5f - (i - 1), 0), Quaternion.identity);
+                            }
+                            //右斜め下
+                            if (dungeonMap[i + 1, j + 1] != "6" && j != 16)
+                            {
+                                dungeonMap[i + 1, j + 1] = "1";
+                                Instantiate(Player1Square, new Vector3(-7.5f + (j + 1), 3.5f - (i + 1), 0), Quaternion.identity);
+                            }
+                        }
+                    }
+                }
                 break;
             case 6:
 
@@ -138,5 +220,146 @@ public class Map : MonoBehaviour
         }
 
         return paint;
+    }
+
+    //横山加筆
+    public bool paintBlueMap(Vector2 pos)
+    {
+        bool paint = false;
+        Debug.Log("塗り");
+        for (int i = 0; i < LineNumber; i++)
+        {
+            for (int j = 0; j < ColumnNumber; j++)
+            {
+                if (i == pos.y && j == pos.x)
+                {
+                    //隣接したマスに何もなければ何もしない
+                    if (dungeonMap[i, j + 1] == "2" || dungeonMap[i + 1, j] == "2" || dungeonMap[i, j - 1] == "2" || dungeonMap[i - 1, j] == "2" ||
+                        dungeonMap[i, j + 1] == "6" || dungeonMap[i + 1, j] == "6" || dungeonMap[i, j - 1] == "6" || dungeonMap[i - 1, j] == "6")
+                    {
+                        formerData = int.Parse(dungeonMap[i, j]);
+                        //対応した色に塗る
+                        dungeonMap[i, j] = "2";
+                        //横山加筆
+                        if (GameManager.rePaint_p2 >= 1)
+                        {
+                            GameManager.rePaint_p2--;
+                        }
+                        paint = true;
+                    }
+                }
+            }
+        }
+
+        //アイテムと重なった処置
+        switch (formerData)
+        {
+            case 3:
+                gameManager.addMoveCounter();
+                break;
+            case 4:
+                gameManager.addRePaint();
+                break;
+            case 5:
+                //爆弾の処理
+                for (int i = 0; i < LineNumber; i++)
+                {
+                    for (int j = 0; j < ColumnNumber; j++)
+                    {
+                        if (i == pos.y && j == pos.x)
+                        {
+                            //上
+                            if (dungeonMap[i - 1, j] != "6")
+                            {
+                                dungeonMap[i - 1, j] = "2";
+                                Instantiate(Player2Square, new Vector3(-7.5f + j, 3.5f - (i - 1), 0), Quaternion.identity);
+                            }
+                            //下
+                            if (dungeonMap[i + 1, j] != "6")
+                            {
+                                dungeonMap[i + 1, j] = "2";
+                                Instantiate(Player2Square, new Vector3(-7.5f + j, 3.5f - (i + 1), 0), Quaternion.identity);
+                            }
+                            //左
+                            if (dungeonMap[i, j - 1] != "6" && j != 0)
+                            {
+                                dungeonMap[i, j - 1] = "2";
+                                Instantiate(Player2Square, new Vector3(-7.5f + (j - 1), 3.5f - i, 0), Quaternion.identity);
+                            }
+                            //左斜め上
+                            if (dungeonMap[i - 1, j - 1] != "6" && j != 0)
+                            {
+                                dungeonMap[i - 1, j - 1] = "2";
+                                Instantiate(Player2Square, new Vector3(-7.5f + (j - 1), 3.5f - (i - 1), 0), Quaternion.identity);
+                            }
+                            //左斜め下
+                            if (dungeonMap[i + 1, j - 1] != "6" && j != 0)
+                            {
+                                dungeonMap[i + 1, j - 1] = "2";
+                                Instantiate(Player2Square, new Vector3(-7.5f + (j - 1), 3.5f - (i + 1), 0), Quaternion.identity);
+                            }
+                            //右
+                            if (dungeonMap[i, j + 1] != "6" && j != 16)
+                            {
+                                dungeonMap[i, j + 1] = "2";
+                                Instantiate(Player2Square, new Vector3(-7.5f + (j + 1), 3.5f - i, 0), Quaternion.identity);
+                            }
+                            //右斜め上
+                            if (dungeonMap[i - 1, j + 1] != "6" && j != 16)
+                            {
+                                dungeonMap[i - 1, j + 1] = "2";
+                                Instantiate(Player2Square, new Vector3(-7.5f + (j + 1), 3.5f - (i - 1), 0), Quaternion.identity);
+                            }
+                            //右斜め下
+                            if (dungeonMap[i + 1, j + 1] != "6" && j != 16)
+                            {
+                                dungeonMap[i + 1, j + 1] = "2";
+                                Instantiate(Player2Square, new Vector3(-7.5f + (j + 1), 3.5f - (i + 1), 0), Quaternion.identity);
+                            }
+                        }
+                    }
+                }
+                break;
+            case 6:
+
+            default: break;
+        }
+
+        return paint;
+    }
+
+    //横山加筆
+    //アイテムをランダムで生成するためのメソッド
+    void CreateItem(int tate_min, int tate_max, int yoko_min, int yoko_max, string Item)
+    {
+        while(true)
+        {
+            int Tate = Random.Range(tate_min, tate_max);
+            int Yoko = Random.Range(yoko_min, yoko_max);
+
+            //何もマップに置いて無かったら
+            if(dungeonMap[Tate, Yoko] == "0")
+            {
+                dungeonMap[Tate, Yoko] = Item;
+
+                switch (dungeonMap[Tate, Yoko])
+                {
+                    case "3"://ポーション
+                        Instantiate(Item1Square, new Vector3(-7.5f + Yoko, 3.5f - Tate, 0), Quaternion.identity);
+                        break;
+
+                    case "4"://バケツ
+                        Instantiate(Item2Square, new Vector3(-7.5f + Yoko, 3.5f - Tate, 0), Quaternion.identity);
+                        break;
+
+                    case "5"://爆弾
+                        Instantiate(Item3Square, new Vector3(-7.5f + Yoko, 3.5f - Tate, 0), Quaternion.identity);
+                        break;
+                }
+
+                //処理を抜ける
+                break;
+            }
+        }
     }
 }
