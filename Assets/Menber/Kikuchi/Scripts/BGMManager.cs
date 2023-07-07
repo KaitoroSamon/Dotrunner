6bea3b729f9154831be58f1fa4a3e49dbc4f6c3b
kikuchitoya <21c1020024kt@edu.tech.ac.jp>
2023-07-07 11:08:23 +0900

    SE、BGMのマネージャーを改変

---
+++ Assets/Menber/Kikuchi/Scripts/BGMManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMManager : MonoBehaviour
+{
+    public static BGMManager Instance { get => _instance; }
+
+    static BGMManager _instance;
+
+
+    public AudioClip[] Audio_Clip_BGM;
+
+    public float[] Audio_Clip_BGM_Vol;
+
+
+    public AudioSource Audio_Source_BGM;
+
+    //float volume;
+
+    public enum BGM_TYPE
+    {
+        TITLE = 0,
+        STAGESELECT = 1,
+        PLAY = 2,
+        END = 3,
+    }
+    BGM_TYPE bgmType = BGM_TYPE.TITLE;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        //Ç±Ç±Ç≈ó¨Ç∑èàóù
+        Play(bgmType);
+
+    }
+    public void Play(BGM_TYPE clip)
+    {
+        Audio_Source_BGM.volume = Audio_Clip_BGM_Vol[(int)clip];
+        Audio_Source_BGM.clip = Audio_Clip_BGM[(int)clip];
+        Audio_Source_BGM.Play();
+
+    }
+
+}
---
+++ Assets/Menber/Kikuchi/Scripts/SEManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEManager : MonoBehaviour
+{
+    public static SEManager Instance { get => _instance; }
+
+    static SEManager _instance;
+
+    
+    public AudioClip[] Audio_Clip_SE;
+
+    public AudioSource Audio_Source_SE;
+
+
+    public enum SE_TYPE
+    {
+        BOMB = 0,
+        PORTION = 1,
+        BUCKET = 2,
+    }
+    //BGM_TYPE bgmType = BGM_TYPE.TITLE;
+    private void Start()
+    {
+        _instance = this;
+     
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Play(SE_TYPE clip)
+    {
+        if(Audio_Clip_SE[(int)clip] != null)
+        {
+            Audio_Source_SE.volume = 1;
+            Audio_Source_SE.clip = Audio_Clip_SE[(int)clip];
+            Audio_Source_SE.PlayOneShot(Audio_Clip_SE[(int)clip]);
+        }
+        else
+        {
+            Debug.Log("éwíËÇ≥ÇÍÇΩSEÇ™Ç†ÇËÇ‹ÇπÇÒÅB");
+        }
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Play(SE_TYPE.BOMB);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Play(SE_TYPE.BUCKET);
+        }
+    }
+
+
+}
